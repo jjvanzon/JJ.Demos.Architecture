d437865bd78b2d5dad2346aed92ab2e5490310e3
jjvanzon <janjoostvanzon@hotmail.com>
2023-01-27 23:46:24 +0100

    📃 Business layer: Enum and EnumExtensions.

---
+++ Business/Enums/EnumExtensions.cs
@@ -0,0 +1,15 @@
+﻿using JJ.Demos.Architecture.Data.Entities;
+using JJ.Framework.Exceptions.Basic;
+
+namespace JJ.Demos.Architecture.Business.Enums;
+
+public static class EnumExtensions
+{
+    public static ProductTypeEnum GetProductTypeEnum(this Product product)
+    {
+        if (product == null) throw new NullException(() => product);
+        if (product.ProductType == null) return ProductTypeEnum.Undefined;
+
+        return (ProductTypeEnum)product.ProductType.ID;
+    }
+}
---
+++ Business/Enums/ProductTypeEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace JJ.Demos.Architecture.Business.Enums;
+
+public enum ProductTypeEnum 
+{
+    Undefined = 0,
+    Physical = 1,
+    Digital = 2
+}
