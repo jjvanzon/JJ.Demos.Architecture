5f66e55b0ff7e6cc07a36c4172983dc3560e8b1a
jjvanzon <janjoostvanzon@hotmail.com>
2023-01-27 23:46:59 +0100

    🌳 Business layer: Visitor demo.

---
+++ Business/Visitors/PolymorphicVisitor.cs
@@ -0,0 +1,71 @@
+﻿using JJ.Demos.Architecture.Business.Enums;
+using JJ.Demos.Architecture.Data.Entities;
+using JJ.Framework.Exceptions.InvalidValues;
+
+namespace JJ.Demos.Architecture.Business.Visitors;
+
+public abstract class PolymorphicVisitorBase
+{
+    protected virtual void VisitOrder(Order order)
+    {
+        VisitPartyPolymorphic(order.Customer);
+        VisitPartyPolymorphic(order.Supplier);
+
+        foreach (OrderLine orderLine in order.OrderLines)
+        {
+            VisitOrderLine(orderLine);
+        }
+    }
+
+    protected virtual void VisitPartyPolymorphic(Party party)
+    {
+        switch (party)
+        {
+            case Supplier x:
+                VisitSupplier(x);
+                break;
+
+            case Customer x:
+                VisitCustomer(x);
+                break;
+
+            default: throw new ArgumentOutOfRangeException(nameof(party));
+        }
+    }
+
+    protected virtual void VisitSupplier(Supplier supplier)
+        => VisitPartyBase(supplier);
+
+    protected virtual void VisitCustomer(Customer customer)
+        => VisitPartyBase(customer);
+
+    protected virtual void VisitPartyBase(Party party) { }
+
+    protected virtual void VisitOrderLine(OrderLine orderLine)
+        => VisitProductPolymorphic(orderLine.Product);
+
+    protected virtual void VisitProductPolymorphic(Product product)
+    {
+        ProductTypeEnum productTypeEnum = product.GetProductTypeEnum();
+        switch (productTypeEnum)
+        {
+            case ProductTypeEnum.Physical:
+                VisitPhysicalProduct(product);
+                break;
+
+            case ProductTypeEnum.Digital:
+                VisitDigitalProduct(product);
+                break;
+
+            default: throw new ValueNotSupportedException(() => productTypeEnum);
+        }
+    }
+
+    protected virtual void VisitPhysicalProduct(Product product)
+        => VisitProductBase(product);
+
+    protected virtual void VisitDigitalProduct(Product product)
+        => VisitProductBase(product);
+
+    protected virtual void VisitProductBase(Product product) { }
+}
