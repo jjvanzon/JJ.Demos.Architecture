147e777e5af094e4ec18aa59cc4302b5d97d3f3e
jjvanzon <janjoostvanzon@hotmail.com>
2023-01-28 19:06:43 +0100

    🌲 Business layer: VisitorBase (without polymorphism).

---
+++ Business/Visitors/VisitorBase.cs
@@ -0,0 +1,25 @@
+﻿using JJ.Demos.Architecture.Data.Entities;
+
+namespace JJ.Demos.Architecture.Business.Visitors;
+
+class VisitorBase
+{
+    protected virtual void VisitOrder(Order order)
+    {
+        VisitCustomer(order.Customer);
+        VisitSupplier(order.Supplier);
+
+        foreach (var orderLine in order.OrderLines)
+        {
+            VisitOrderLine(orderLine);
+        }
+    }
+
+    protected virtual void VisitCustomer(Customer customer) { }
+    protected virtual void VisitSupplier(Supplier supplier) { }
+
+    protected virtual void VisitOrderLine(OrderLine orderLine)
+        => VisitProduct(orderLine.Product);
+
+    protected virtual void VisitProduct(Product product) { }
+}
