b2f2fe5df5a5e00408192a90ee551c68294d284c
jjvanzon <janjoostvanzon@hotmail.com>
2023-01-30 00:14:46 +0100

    Business: Visitors: ReversedOrderVisitor

---
+++ Business/Visitors/ReversedOrderVisitor.cs
@@ -0,0 +1,23 @@
+﻿using JJ.Demos.Architecture.Business.Visitors;
+using JJ.Demos.Architecture.Data.Entities;
+
+/// <summary>
+/// This Visitor changes the order of processing.
+/// </summary>
+class ReversedOrderVisitor : OrderVisitorBase
+{
+    protected override void VisitOrder(Order order)
+    {
+        foreach (var orderLine in order.OrderLines.ToArray())
+        {
+            VisitOrderLine(orderLine);
+        }
+
+        // Visit Customer and Supplier last instead of first.
+        VisitCustomer(order.Customer);
+        VisitSupplier(order.Supplier);
+    }
+}
+
+
+
