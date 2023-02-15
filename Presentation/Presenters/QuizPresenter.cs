3bf3274910427918e0c6e37cca449d13bc072a1b
jjvanzon <janjoostvanzon@hotmail.com>
2023-02-15 20:17:10 +0100

    🧹 Presentation: ViewModels: Quiz ViewModel / Presenter: Removing ellipsis ...

--- Presentation/Presenters/QuizPresenter.cs
+++ Presentation/Presenters/QuizPresenter.cs
@@ -5,8 +5,11 @@ public class QuizPresenter
     public QuizViewModel Answer(QuizViewModel userInput)
     {
         var viewModel = new QuizViewModel();
+
+        // Yield over non-persisted properties.
         viewModel.SelectedOption = userInput.SelectedOption;
         viewModel.AnswerVisible = userInput.AnswerVisible;
+
         return viewModel;
     }
 }
