14cfe3d1dafe6fbe0e30ef806a1309afde338708
jjvanzon <janjoostvanzon@hotmail.com>
2023-02-16 15:26:16 +0100

    🖥️ Presentation: ViewModels: ScreenViewModelBase_2_Clean

---
+++ Presentation/ViewModels/Screens/ScreenViewModelBase_2_Clean/ScreenViewModelBase.cs
@@ -0,0 +1,9 @@
+﻿namespace JJ.Demos.Architecture.Presentation.ViewModels.Screens.
+          ScreenViewModelBase_2_Clean;
+
+public abstract class ScreenViewModelBase
+{
+    public bool Visible { get; set; }
+    public bool Successful { get; set; }
+    public IList<string> ValidationMessages { get; set; }
+}
