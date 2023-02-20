90bc04a0c246c1a816cd5366d60f277fae7f2f0c
jjvanzon <janjoostvanzon@hotmail.com>
2023-02-20 22:22:25 +0100

    🥼 Presentation: ProductEditPresenter Tests: Clean away some more exceptions.

--- Data/DefaultRepositories/ProductRepository.cs
+++ Data/DefaultRepositories/ProductRepository.cs
@@ -3,7 +3,7 @@
 namespace JJ.Demos.Architecture.Data.DefaultRepositories;
 
 [UsedImplicitly]
-public class ProductRepository : RepositoryBase<Product, int>
+public class ProductRepository : RepositoryBase<Product, int>, IProductRepository
 {
     public ProductRepository(IContext context) : base(context) { }
 }
--- Data/GlobalUsings.cs
+++ Data/GlobalUsings.cs
@@ -1,4 +1,5 @@
 // Global using directives
 
 global using JJ.Demos.Architecture.Data.Entities;
+global using JJ.Demos.Architecture.Data.RepositoryInterfaces;
 global using JJ.Framework.Data;
