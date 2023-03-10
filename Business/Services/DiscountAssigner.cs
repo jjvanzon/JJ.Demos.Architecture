9122adfbbcf74b5f347e1e23979f59a13bf1af2d
jjvanzon <janjoostvanzon@hotmail.com>
2023-03-10 22:49:03 +0100

    📀 Changed DiscountAssigner code sample for ORM.

--- Business/Services/DiscountAssigner.cs
+++ Business/Services/DiscountAssigner.cs
@@ -2,17 +2,14 @@
 
 public class DiscountAssigner
 {
-    public void AssignDiscounts(Order order)
+    void AssignDiscounts(Order order)
     {
-        // Get order lines with a product price at least 0 and at most 100.
-        IList<OrderLine> orderLines =
+        var collection =
             order.OrderLines
                  .Where(x => x.Product.Price >= 10)
-                 .Where(x => x.Product.Price <= 100)
-                 .ToList();
+                 .Where(x => x.Product.Price <= 100);
 
-        // Fill in a discount percentage to get 10% off on these products.
-        foreach (OrderLine orderLine in orderLines)
+        foreach (OrderLine orderLine in collection)
         {
             orderLine.DiscountPercentage = 10;
         }
