2de9646ed559c688ed9562582dcc84186e9568ec
jjvanzon <janjoostvanzon@hotmail.com>
2023-03-15 13:59:19 +0100

    🏭 Factory demo

---
+++ Business/Factory/Factory.cs
@@ -0,0 +1,24 @@
+﻿namespace JJ.Demos.Architecture.Business.Factory;
+
+public static class ThingFactory
+{
+    public static IThing CreateThing(int parameter)
+    {
+        switch (parameter)
+        {
+            case 0:
+                return new NormalThing();
+            case 1:
+                return new SpecialThing();
+            default:
+                throw new Exception(
+                    $"parameter value '{parameter}' is not supported.");
+        }
+    }
+}
+
+public class SpecialThing : IThing { }
+
+public class NormalThing : IThing { }
+
+public interface IThing { }
