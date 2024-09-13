196c86a416e73670d62c658898ea36fbf8a741b1
jjvanzon <jjvanzon79@gmail.com>
2024-09-13 23:08:45 +0200

    🏭 Factory pattern modified

--- Business/Factory/Factory.cs
+++ Business/Factory/Factory.cs
@@ -1,24 +1,20 @@
 ﻿namespace JJ.Demos.Architecture.Business.Factory;
 
-public static class ThingFactory
+interface IThing { }
+class NormalThing : IThing { }
+class SpecialThing : IThing { }
+
+static class ThingFactory
 {
-    public static IThing CreateThing(int parameter)
+    static IThing CreateThing(int parameter)
     {
         switch (parameter)
         {
-            case 0:
-                return new NormalThing();
-            case 1:
-                return new SpecialThing();
-            default:
-                throw new Exception(
-                    $"parameter value '{parameter}' is not supported.");
+            case 0: return new NormalThing();
+
+            case 1: return new SpecialThing();
+
+            default: throw new NotSupportedException();
         }
     }
 }
-
-public class SpecialThing : IThing { }
-
-public class NormalThing : IThing { }
-
-public interface IThing { }
