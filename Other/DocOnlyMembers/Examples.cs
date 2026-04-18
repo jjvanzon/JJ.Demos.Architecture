51faff4b5610171a54d0972a38e62cfbb817ab42
jjvanzon <jjvanzon79@gmail.com>
2026-04-18 18:06:42 +0200

    🚸 DocOnlyMembers - Move paragraph 1 examples to separate file. Squelch warnings in csproj(.DotSettings) that aren't to do with the docs themselves, just syntax lenience for the examples.

--- Other/DocOnlyMembers/Examples.cs
+++ Other/DocOnlyMembers/Examples.cs
@@ -25,26 +25,6 @@ class WithoutDocComment
     }
  }
 
-class WithDocComment
-{
-    /// <summary>
-    /// Changes the first character to upper case:
-    /// <code>
-    /// StartWithCap("test") = "Test"
-    /// </code>
-    /// </summary>
-    string StartWithCap(string input)
-    {
-        if (input.Length == 0) return input;
-        return input.Left(1).ToUpper() + input.CutLeft(1);
-    }
-
-    void MyCode()
-    {
-        var output = StartWithCap("test");
-    }
-}
-
 class CrowdedComments
 {
     /// <summary>
---
+++ Other/DocOnlyMembers/Paragraph1_WhatAreXmlDocComments.cs
@@ -0,0 +1,21 @@
+﻿namespace JJ.Demos.Architecture;
+
+class WithDocComment
+{
+    void MyCode()
+    {
+        var output = StartWithCap("test");
+    }
+
+    /// <summary>
+    /// Changes the first character to upper case:
+    /// <code>
+    /// StartWithCap("test") = "Test"
+    /// </code>
+    /// </summary>
+    string StartWithCap(string input)
+    {
+        if (input.Length == 0) return input;
+        return input.Left(1).ToUpper() + input.CutLeft(1);
+    }
+}
